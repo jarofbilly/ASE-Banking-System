8c87761b9bd1a5fcb8760a5210d2113b5bf1dd5f
Billy Street-Rose <60233676+jarofbilly@users.noreply.github.com>
2023-03-29 00:00:34 +0100

    Adds user creation functionality

--- program.py
+++ program.py
@@ -2,6 +2,9 @@ import tkinter as tk
 import tkinter.messagebox
 from tkinter import font as tkFont
 import csv
+from random import randint
+
+userDb = None
 
 class bButton(tk.Button):
     def __init__(self, parent=None, **config):
@@ -40,22 +43,54 @@ def checkLogin(username, password):
                     return userDbEntry(row)
         return False
 
-userDb = None
+def createLogin(username, password):
+    with open('db/data.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        newId = None
+
+        uniqueIdFound = False
+        while not uniqueIdFound:
+            newId = randint(10000000, 99999999)
+
+            isUnique = True
+            for row in reader:
+                if row['username'] == username:
+                    return False
+                if row['id'] == newId:
+                    isUnique = False
+                    break
+            
+            if isUnique:
+                uniqueIdFound = True
+
+    newUserData = [newId, username, password, 0]
+    with open('db/data.csv', 'a') as csvfile:
+        writer_object = csv.writer(csvfile)
+        writer_object.writerow(newUserData)
+    
+    return True
 
 def handleLogin():
     result = checkLogin(userField.get(), pwField.get())
-    userField
+
     if not result:
-        tkinter.messagebox.showerror(title="", message="Incorrect username or password!")
+        tkinter.messagebox.showerror(title="", message="Incorrect username or password!\nIf you have not yet created an account, press create one below.")
     else:
         userDb = result
         tkinter.messagebox.showinfo(title="", message="Logged in successfully!")
+    
     userField.delete(0, 'end')
     pwField.delete(0, 'end')
 
+def handleCreate():
+    result = createLogin(userField.get(), pwField.get())
+    if not result:
+        tkinter.messagebox.showerror(title="", message="That username is already taken. Please choose another.")
+    else:
+        tkinter.messagebox.showinfo(title="", message="Account created successfully!\nPlease press login.")
 
 loginButton = bButton(loginFrame, text="Login", command=handleLogin)
-createButton = bButton(loginFrame, text="Create an account")
+createButton = bButton(loginFrame, text="Create an account", command=handleCreate)
 
 userLabel.grid(row=0, column=0, pady=2)
 userField.grid(row=0, column=1, pady=2)
