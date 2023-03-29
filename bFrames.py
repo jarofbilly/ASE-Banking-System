9dd89653c49224560eaaab7bbeac910d06e387b4
Billy Street-Rose <60233676+jarofbilly@users.noreply.github.com>
2023-03-29 13:49:03 +0100

    Home screen and db passing

--- bFrames.py
+++ bFrames.py
@@ -5,6 +5,7 @@ from bUtilities import *
 class LoginFrame(tk.Frame):
     def __init__(self, parent=None, db=None, **config):
         tk.Frame.__init__(self, parent, config)
+        self.userDb = None
 
         def handleLogin():
             result = db.checkLogin(userField.get(), pwField.get())
@@ -12,11 +13,12 @@ class LoginFrame(tk.Frame):
             if not result:
                 tkinter.messagebox.showerror(title="", message="Incorrect username or password!\nIf you have not yet created an account, press create one below.")
             else:
-                userDb = result
+                self.userDb = result
                 tkinter.messagebox.showinfo(title="", message="Logged in successfully!")
             
             userField.delete(0, 'end')
             pwField.delete(0, 'end')
+            self.destroy()
 
         def handleCreate():
             result = db.createLogin(userField.get(), pwField.get())
@@ -42,6 +44,31 @@ class LoginFrame(tk.Frame):
         pwField.grid(row=1, column=1, pady=2)
         loginButton.grid(row=3, column=0, columnspan=2, pady=2, sticky="ew")
         createButton.grid(row=4, column=0, columnspan=2, pady=2, sticky="ew")
+
+class HomeFrame(tk.Frame):
+    def __init__(self, parent=None, userDb=None, **config):
+        tk.Frame.__init__(self, parent, config)
+        self.userDb = userDb
+
+        def handleDeposit():
+            tkinter.messagebox.showinfo(title="", message="Task completed successfully!")
+
+        def handleLogout():
+            self.destroy()
+
+        # Element creation.
+        depositButton = bButton(self, text="Deposit", command=handleDeposit)
+        withdrawButton = bButton(self, text="Withdraw", command=handleDeposit)
+        transferButton = bButton(self, text="Transfer", command=handleDeposit)
+        detailsButton = bButton(self, text="View summary", command=handleDeposit)
+        logoutButton = bButton(self, text="Log out", command=handleLogout)
+
+        # Placement.
+        depositButton.grid(row=0, column=0, columnspan=2, pady=2, sticky='ew')
+        withdrawButton.grid(row=1, column=0, columnspan=2, pady=2, sticky='ew')
+        transferButton.grid(row=2, column=0, columnspan=2, pady=2, sticky='ew')
+        detailsButton.grid(row=3, column=0, columnspan=2, pady=2, sticky='ew')
+        logoutButton.grid(row=4, column=0, columnspan=2, pady=2, sticky='ew')
 
 class DepositFrame(tk.Frame):
     def __init__(self, parent=None, db=None, **config):
--- program.py
+++ program.py
@@ -8,9 +8,14 @@ csvIO = dbIO('db/data.csv')
 root = tk.Tk()
 root.geometry("600x400")
 
-#loginFrame = LoginFrame(root, csvIO)
-#loginFrame.place(relx=0.5, rely=0.5, anchor="c")
 
-DepositFrame(root, csvIO).place(relx=0.5, rely=0.5, anchor="c")
+loginFrame = LoginFrame(root, csvIO)
+loginFrame.place(relx=0.5, rely=0.5, anchor="c")
+root.wait_window(loginFrame)
+userDb = loginFrame.userDb
+
+homeFrame = HomeFrame(root, userDb)
+homeFrame.place(relx=0.5, rely=0.5, anchor="c")
+root.wait_window(homeFrame)
 
 root.mainloop()
